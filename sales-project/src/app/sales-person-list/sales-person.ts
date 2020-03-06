4111633f258ffcd358f2673e274f2b98648ab07d
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-03-06 15:47:23 +0100

    Change data type of sales volume from str to num

--- sales-project/src/app/sales-person-list/sales-person.ts
+++ sales-project/src/app/sales-person-list/sales-person.ts
@@ -3,6 +3,6 @@ export class SalesPerson {
     public firstName: string,
     public lastName: string,
     public email: string,
-    public salesVolume: string
+    public salesVolume: number
   ) {}
 }
