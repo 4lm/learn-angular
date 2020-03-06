4ee863f3e22c6434ad41f3ca318551026e40feb9
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-03-06 16:51:28 +0100

    Use angular material table

--- sales-project/src/app/app.module.ts
+++ sales-project/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { AppComponent } from "./app.component";
+import { SalesPersonListComponent } from "./sales-person-list/sales-person-list.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SalesPersonListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SalesPersonListComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { SalesPerson } from "./sales-person";
-import { getMaxListeners } from "cluster";
 
 @Component({
   selector: "app-sales-person-list",
@@ -8,14 +7,20 @@ import { getMaxListeners } from "cluster";
   styleUrls: ["./sales-person-list.component.scss"]
 })
 export class SalesPersonListComponent implements OnInit {
-  // Create an array of sales person objects
-  salesPersonList: SalesPerson[] = [
+  displayedColumns: string[] = [
+    "firstName",
+    "lastName",
+    "email",
+    "salesVolume"
+  ];
+  dataSource: SalesPerson[] = [
     new SalesPerson("Alice", "Allison", "aliallison@gmail.com", 245364),
     new SalesPerson("Bob", "Schwartz", "bschwartz@gmail.com", 309863),
     new SalesPerson("John", "Doe", "jd@gmail.com", 132545),
     new SalesPerson("Jane", "Dow", "jdow@gmail.com", 99949494),
     new SalesPerson("Mad", "Max", "mm@gmail.com", 0)
   ];
+
   constructor() {}
 
   ngOnInit(): void {}
