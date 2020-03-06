82955a7501f42d2c2c8916eca3a9dd3374dfb1c4
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-03-06 17:26:58 +0100

    Switch to material components

--- sales-project/src/app/app.module.ts
+++ sales-project/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from "./app.component";
 import { SalesPersonListComponent } from "./sales-person-list/sales-person-list.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [AppComponent, SalesPersonListComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
